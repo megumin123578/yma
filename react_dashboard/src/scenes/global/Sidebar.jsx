baf510c883adac746ab132b1f83d4e63e9d8a803
megumin123578 <saranghae123578@gmail.com>
2025-12-22 16:51:21 +0700

    update name

--- react_dashboard/src/scenes/global/Sidebar.jsx
+++ react_dashboard/src/scenes/global/Sidebar.jsx
@@ -168,7 +168,7 @@ const Sidebar = () => {
               color={colors.grey[300]}
               sx={{ m: "15px 0 5px 20px" }}
             >
-              Data
+              Analytics
             </Typography>
 
             <Item
