567098717a8c39de864595449154d21a184285f5
megumin123578 <saranghae123578@gmail.com>
2026-01-29 14:10:34 +0700

    udpate helper

---
+++ python_backend/install_requirements.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pip install uvicorn
+pip install fastapi
+pip install sqlalchemy
+pip install psycopg2
+pip install python-jose[cryptography]
+pip install passlib
+pip install google-auth google-auth-oauthlib google-auth-httplib2
+pip install google-api-python-client
+pip install bs4
+pip install python-multipart
